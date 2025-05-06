ac426169f3a9b38997a919ee26d19f0478159ad5
Oscar <oscar.dodd1@education.nsw.gov.au>
2025-05-06 13:06:51 +1000

    Updated main.py
    -Added distance detection
    -Added moving to block

--- 9CT_Mechatronics_Project/main.py
+++ 9CT_Mechatronics_Project/main.py
@@ -16,6 +16,10 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 colour_sensor = ColorSensor(Port.S3)
 ultrasonic_sensor = UltrasonicSensor(Port.S2)
 
+#Settings
+detect_distance = 150 #150mm
+check_block_distance = 5 #5mm
+
 #sensor_motor.run_angle(90, 90)
 #sensor_motor.run_angle(90, -90)
 
@@ -39,9 +43,40 @@ ultrasonic_sensor = UltrasonicSensor(Port.S2)
     #robot.straight(-300)
     #robot.turn(120)
 
-while True:
-    current_color = colour_sensor.color()
+#Start
 
+#block finding and moving
+while True:
+    #finding block
+    counter = 60
+    found_block = False
+    while counter > 0 and found_block == False:
+        ev3.screen.print(counter)
+        if ultrasonic_sensor.distance() < detect_distance:
+            found_block = True
+            ev3.screen.print("Block found")
+
+        counter -= 1
+        robot.turn(3)
+        ev3.screen.clear()
+
+    #go to block
+    while ultrasonic_sensor.distance() > check_block_distance:
+        ev3.screen.print(ultrasonic_sensor.distance())
+        robot.straight(5) #move forward 5mm
+        ev3.screen.clear()
+    
+    #check block colour
+    current_color = colour_sensor.color()
     ev3.screen.print(current_color)
-    wait(1)
+
+    if current_color == Color.RED or Color.YELLOW:
+        ev3.screen.print("Good block found")
+        ev3.screen.print(current_color)
+        ev3.screen.clear()
+        #take block back to the start
+
     ev3.screen.clear()
+
+#end
+ev3.screen.print("Finished")
