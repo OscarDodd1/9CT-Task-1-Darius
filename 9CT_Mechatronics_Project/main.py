91b0edf645cbb966354ae93dcf7494a85c218c0c
Oscar <oscar.dodd1@education.nsw.gov.au>
2025-05-12 12:30:53 +1000

    Updater
    -main.pyu

--- 9CT_Mechatronics_Project/main.py
+++ 9CT_Mechatronics_Project/main.py
@@ -13,12 +13,15 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-colour_sensor = ColorSensor(Port.S3)
 ultrasonic_sensor = UltrasonicSensor(Port.S2)
 
 #Settings
-detect_distance = 150 #150mm
-check_block_distance = 5 #5mm
+detect_distance = 300 #150mm
+check_block_distance = 80 #5mm
+
+#variables
+red_block = False
+yellow_block = False
 
 #Start
 
@@ -28,24 +31,28 @@ while True:
     counter = 60
     found_block = False
     while counter > 0 and found_block == False:
+        ev3.screen.print("Finding block")
         ev3.screen.print(counter)
-        if ultrasonic_sensor.distance() < detect_distance:
-            found_block = True
-            ev3.screen.print("Block found")
 
         counter -= 1
         robot.turn(3)
         ev3.screen.clear()
 
-    if found_block:
-        #go to block
-        found_block = False
-        while ultrasonic_sensor.distance() > check_block_distance:
-            ev3.screen.print(ultrasonic_sensor.distance())
-            robot.straight(5) #move forward 5mm
-            ev3.screen.clear()
+        if ultrasonic_sensor.distance() < detect_distance:
+            found_block = True
+            ev3.screen.print("Block found")
+            break
+
+    #go to block
+    while ultrasonic_sensor.distance() > check_block_distance:
+        ev3.screen.print("Going to block")
+        ev3.screen.print(ultrasonic_sensor.distance())
+        robot.straight(2.5) #move forward 5mm
+        ev3.screen.clear()
     
-        #check block colour
+    #check block colour
+    while True:
+        ev3.screen.print("Checking block colour")
         current_color = colour_sensor.color()
         ev3.screen.print(current_color)
 
@@ -54,6 +61,16 @@ while True:
             ev3.screen.print(current_color)
             ev3.screen.clear()
             #take block back to the start\
+            while True:
+                ev3.screen.print("Taking " + current_color + " block back")
+                wait(10)
+                ev3.screen.clear()
+        else:
+            ev3.screen.print("Bad block found")
+            ev3.screen.print(current_color)
+            ev3.screen.clear()
+
+        ev3.screen.clear()
 
     wait(10)
     ev3.screen.clear()
